3175d1cdf63bdb7f53b0d746397ded3670f54175
Chris Fung <aergonaut@gmail.com>
2024-02-12 21:10:00 -0800

    allow disabling registrations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def flash_key_to_color(key)
+    case key.to_sym
+    when :notice
+      "blue"
+    when :alert
+      "red"
+    when :success
+      "green"
+    end
+  end
 end
