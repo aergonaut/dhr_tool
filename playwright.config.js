75681cdac49e2cac1115a13fddabc63205178523
Chris Fung <aergonaut@gmail.com>
2024-02-22 22:17:18 -0800

    add web server start command

--- playwright.config.js
+++ playwright.config.js
@@ -70,9 +70,9 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'CYPRESS=1 bundle exec rails s -p 5017 -e test',
+    url: 'http://127.0.0.1:5017/up',
+    reuseExistingServer: !process.env.CI,
+  },
 });
