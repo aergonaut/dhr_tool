bf50f270c38a967e16858af5d69f8f340d5c6595
Chris Fung <aergonaut@gmail.com>
2024-02-22 23:13:17 -0800

    setup playwright to run on CI (#3)
    * add cypress-on-rails
    
    * adding playwright e2e tests
    
    * setup playwright to run on CI
    
    * fix lint
    
    * fix playwright image tag
    
    * add web server start command
    
    * install ruby
    
    * set up db schema before playwright
    
    * run spec:prepare to build tailwind assets
    
    * TYPO

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  config.dsn = "https://9faa29bb8a3b656d7ad1711203d00307@o4506295251173376.ingest.sentry.io/4506765652656128"
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+unless Rails.env.development?
+  Sentry.init do |config|
+    config.dsn = "https://9faa29bb8a3b656d7ad1711203d00307@o4506295251173376.ingest.sentry.io/4506765652656128"
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  # Set traces_sample_rate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  # We recommend adjusting this value in production.
-  config.traces_sample_rate = 0.2
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    config.traces_sample_rate = 0.2
+  end
 end
---
+++ spec/playwright/e2e/rails_examples/sessions.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+import { app, appFactories } from '../../support/on-rails';
+
+test.describe("Sign in flow", () => {
+  test.beforeEach(async ({ page }) => {
+    await app('clean');
+  });
+
+  test("Logging in as a confirmed user", async ({ page }) => {
+    const email = "example@example.com"
+    const password = "swordfish"
+    await appFactories([["create", "user", "confirmed", {email, password}]]);
+    await page.goto("/");
+
+    await page.getByLabel("Email").fill(email);
+    await page.getByLabel("Password").fill(password);
+  
+    await page.getByRole("button", { name: "Sign in"}).click();
+
+    await page.waitForURL("**/tools/links");
+  });
+});
